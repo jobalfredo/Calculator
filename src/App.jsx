dc4f81719c4e02a3408f8d20e90866cef6b1307b
jobalfredo <jobalfredo@gmail.com>
2025-02-18 02:13:12 +0800

    Fileadded

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Calculator from './components/Calculator';
+import Cat from './components/output-onlinegiftools.gif';
+
+const App = () => {
+  return (
+    <div className='App'> 
+
+      <Calculator />
+      <h1>Calculator</h1>
+      <p>By Job</p>
+    </div>
+
+  )
+}
+
+export default App
